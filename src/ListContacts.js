a22c6ec0ef7257aeafaef41d86115b769efc660c
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-08-29 14:45:23 -0700

    added input box as a controlled component for search input

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,27 +1,46 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 
-function ListContacts(props) {
-return (
-        <ol className='contact-list'>
-          {props.contacts.map((contact) => (
-            <li key={contact.id} className='contact-list-item'>
-              <img className='contact-avatar' src={contact.avatarURL} alt='' />
-              <div className='contact-details'>
-                {contact.name}<br/>
-                {contact.email}
+
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query.trim()
+    })
+  }
+  render() {
+    return (
+           <div className='list-contacts'>
+            <div className='list-contacts-top'>
+              <input className='search-contacts' type='text' placeholder='Search Contacts'
+                value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} />
+            </div>
+            <ol className='contact-list'>
+              {this.props.contacts.map((contact) => (
+                <li key={contact.id} className='contact-list-item'>
+                  <img className='contact-avatar' src={contact.avatarURL} alt='' />
+                  <div className='contact-details'>
+                    {contact.name}<br/>
+                    {contact.email}
+                  </div>
+                  <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                    Remove
+                  </button>
+                </li>
+               ))}
+              </ol>
               </div>
-              <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-                Remove
-              </button>
-            </li>
-           ))}
-          </ol>
-);
-}
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
+    );
+  }
 }
 
 
