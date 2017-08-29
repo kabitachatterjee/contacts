9932af099a7745ad4144cbb69a2a4d6cff3c23ff
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-08-29 15:10:51 -0700

    implemented search with RegExp and sorting contacts alphabetically

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
+import EscapeStringRegex from 'escape-string-regexp';
+import SortBy from 'sort-by';
 
 
 class ListContacts extends Component {
@@ -18,21 +20,35 @@ class ListContacts extends Component {
     })
   }
   render() {
+    const { contacts, onDeleteContact} = this.props
+    const { query } = this.state
+
+    let showingContacts
+    if(query) {
+      const match = new RegExp(EscapeStringRegex(query), 'i')
+      showingContacts = contacts.filter((contact) => (match.test(contact.name)))
+    }
+    else {
+      showingContacts = contacts
+    }
+
+    showingContacts = showingContacts.sort(SortBy('name'))
+
     return (
            <div className='list-contacts'>
             <div className='list-contacts-top'>
               <input className='search-contacts' type='text' placeholder='Search Contacts'
-                value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} />
+                value={query} onChange={(event) => this.updateQuery(event.target.value)} />
             </div>
             <ol className='contact-list'>
-              {this.props.contacts.map((contact) => (
+              {showingContacts.map((contact) => (
                 <li key={contact.id} className='contact-list-item'>
                   <img className='contact-avatar' src={contact.avatarURL} alt='' />
                   <div className='contact-details'>
                     {contact.name}<br/>
                     {contact.email}
                   </div>
-                  <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                  <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
                     Remove
                   </button>
                 </li>
