cc43bb0ea32fdb0296b738751c91fe4ceadc8657
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-08-30 08:43:28 -0700

    added a new component for creating a new contact

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
+
 
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    screen: 'list',
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({
+        contacts: contacts
+      })
+    })
   }
 
   removeContact = (contact) => {
     this.setState((state) => ({
       contacts: this.state.contacts.filter((c) => c.id !== contact.id)
       }))
+      ContactsAPI.remove(contact)
   }
   render() {
     return (
+      <div classNme='app'>
+      {this.state.screen === 'list' && (
       <ListContacts
       onDeleteContact={this.removeContact}
-      contacts={this.state.contacts} />
+      contacts={this.state.contacts} />)}
+      {this.state.screen === 'create' && (
+        <CreateContact />
+      )}
+
+      </div>
     )
   }
 }
